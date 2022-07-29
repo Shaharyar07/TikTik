da31831aff2efb7e2b63d94484948fef7bef6363
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-07-29 17:56:57 +0500

    detail page api created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,13 +16,15 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <GoogleOAuthProvider
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
     >
-      <Navbar />
-      <div className='flex gap-6 md:gap-20'>
-        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-          <Sidebar />
-        </div>
-        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-          <Component {...pageProps} />
+      <div className='xl:w-[1200px] h-[100vh] m-auto overflow-hidden'>
+        <Navbar />
+        <div className='flex gap-6 md:gap-20'>
+          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+            <Sidebar />
+          </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </GoogleOAuthProvider>
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,6 +7,7 @@ import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
 import { SanityAssetDocument } from "@sanity/client";
 import { topics } from "../utils/constants";
+import { BASE_URL } from "../utils";
 
 const Upload = () => {
   const { userProfile }: { userProfile: any } = useAuthStore();
@@ -56,7 +57,7 @@ const Upload = () => {
         },
         topic: topic,
       };
-      await axios.post("http://localhost:3000/api/post", document);
+      await axios.post(`${BASE_URL}/api/post`, document);
       router.push("/");
     }
   };
