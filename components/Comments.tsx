5f9299eeb61a6e2a813760f801d67e10aa66048b
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-07-31 01:19:31 +0500

    fetch all users and fetch comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { GoVerified } from "react-icons/go";
 import useAuthStore from "../store/authStore";
 import NoResults from "./NoResults";
+import { IUser } from "../types";
 interface IProps {
   isPostingComment: boolean;
   comment: string;
@@ -28,12 +29,48 @@ const Comments = ({
   comments,
   isPostingComment,
 }: IProps) => {
-  const { userProfile } = useAuthStore();
+  const { userProfile, allUsers } = useAuthStore();
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
       <div className='overflow-scroll lg:h-[375px]'>
         {comments?.length ? (
-          <div>Videos</div>
+          comments.map((comment, idx) => (
+            <>
+              {allUsers.map(
+                (user: IUser) =>
+                  user._id ===
+                    (comment.postedBy._id ||
+                      user._id === comment.postedBy._ref) && (
+                    <div className='p-2 items-center' key={idx}>
+                      <Link href={`/profile/${user._id}`}>
+                        <div className='flex gap-3 items-start'>
+                          <div className='h-9 w-9'>
+                            <Image
+                              width={62}
+                              height={62}
+                              className='rounded-full'
+                              src={user.image}
+                              alt='profile'
+                              layout='responsive'
+                            />
+                          </div>
+                          <div className='hidden xl:block'>
+                            <p className='flex items-center gap-1 text-md font-bold text-primary lowercase'>
+                              {user.userName}
+                              <GoVerified className='text-blue-400' />
+                            </p>
+                            <p className='capitalize font-medium text-xs text-gray-500'>
+                              {user.userName}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
+                      <p className="m-4">{comment.comment}</p>
+                    </div>
+                  )
+              )}
+            </>
+          ))
         ) : (
           <NoResults text='No Comments yet! Be the first one to add a comment.' />
         )}
@@ -45,7 +82,7 @@ const Comments = ({
               type='text'
               value={comment}
               onChange={(e) => {
-                setComment(e.target.value.trim());
+                setComment(e.target.value);
               }}
               placeholder='Add Comment...'
               className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250ox] md:w-[750px] lg:w-[350px] border-gray-100 focus: outline-none focus:border-2 focus:border-gray-300 flex rounded-lg '
--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import useAuthStore from "../store/authStore";
+import { IUser } from "../types";
 
 const SuggestedAccounts = () => {
-  return <div>SuggestedAccounts</div>;
+  const { fetchAllUsers, allUsers } = useAuthStore();
+  useEffect(() => {
+    fetchAllUsers();
+  }, [fetchAllUsers]);
+
+  return (
+    <div className='xl:border-b-2 border-gray-200 pb-4'>
+      <p className='font-semibold m-3 text-gray-500 mt-4 hidden xl:block'>
+        Suggested Accounts
+      </p>
+      <div>
+        {allUsers.map((user: IUser) => (
+          <Link href={`/profile/${user._id}`} key={user._id}>
+            <div className='flex gap-3 hover:bg-primary p-2 rounded font-semibold cursor-pointer'>
+              <div className='h-9 w-9'>
+                <Image
+                  width={62}
+                  height={62}
+                  className='rounded-full'
+                  src={user.image}
+                  alt='profile'
+                  layout='responsive'
+                />
+              </div>
+              <div className='hidden xl:block'>
+                <p className='flex items-center gap-1 text-md font-bold text-primary lowercase'>
+                  {user.userName}
+                  <GoVerified className='text-blue-400' />
+                </p>
+                <p className='capitalize font-medium text-xs text-gray-500'>
+                  {user.userName}
+                </p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SuggestedAccounts;
