ceddae2af567a6947f767f5fce74e7d372edfb50
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-07-31 22:43:02 +0500

    search frontend added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
@@ -11,7 +12,17 @@ import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
 const Navbar = () => {
+  const router = useRouter();
   const { userProfile, addUser, removeUser } = useAuthStore();
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
@@ -24,7 +35,25 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>SEARCH</div>
+      <div className='relative hidden md:block'>
+        <form
+          onSubmit={handleSubmit}
+          className='absolute md:static top-10 -left-20 bg:white'
+        >
+          <input
+            type='text'
+            value={searchValue}
+            placeholder='Search Accounts and Videos...'
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+            className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0 focus:outline-none'
+          />
+          <button className='absolute md:right-5 right-6 top-3 border-l-2 border-gray-300 pl-4 text-3xl text-gray-400'>
+            <BiSearch />
+          </button>
+        </form>
+      </div>
       <div>
         {userProfile ? (
           <div className='flex gap-5 md:gap-10'>
